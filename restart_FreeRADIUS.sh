1cd4df1bbffadf43c1ad66f206fd7749eb0d7bd6
di-shi <>
2009-01-30 02:37:49 +0000

    1. Added OSP module configuration file. 2. Added restart script for FreeRADIUS with OSP module.

---
+++ restart_FreeRADIUS.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+#
+# restart_FreeRADIUS.sh
+#
+# This script checks if FreeRADIUS is running.
+# If the application is not running, it restarts it.
+#
+# This script must be run by a user root
+#
+
+if pgrep radiusd >/dev/null 2>&1
+    then
+    # Do nothing, the radius server is running
+    exit 1
+fi
+
+echo "!!!FreeRADIUS is not running!!!"
+echo "The application will be restarted"
+/usr/local/sbin/radiusd -fx &
+
